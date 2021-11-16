ec4acee194c6aa1ef76ebef8658bc4309030f626
Idris Bankole <bankoleidris@gmail.com>
2021-11-16 15:56:36 +0100

    declaring question types

--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,15 @@
+import { shuffleArray } from "./utils";
+
 export type Question = {
   category: string;
   correct_answer: string;
   difficulty: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
   question: string;
   type: string;
 };
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = "easy",
@@ -20,7 +24,13 @@ export const fetchQuizQuestions = async (
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   try {
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
+    return data.results.map((question: Question) => ({
+      ...question,
+      answer: shuffleArray([
+        ...question.incorrect_answers,
+        question.correct_answer,
+      ]),
+    }));
   } catch (e) {
     console.log(e);
   }
