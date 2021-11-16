7dc5eec7fd593b661ccdf9392f344f42d893adf5
Idris Bankole <bankoleidris@gmail.com>
2021-11-16 07:12:23 +0100

    question cards

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  useranswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
 function QuestionCard() {
   return <div>Question Card</div>;
 }
