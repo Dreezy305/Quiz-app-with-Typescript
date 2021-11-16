cc57392e7996a127af8ffc913e11864f227bd0d6
Idris Bankole <bankoleidris@gmail.com>
2021-11-16 08:18:18 +0100

    server side fetch

--- src/API.ts
+++ src/API.ts
@@ -7,4 +7,12 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
-) => {};
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  try {
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
