48e001a2bbcd7e3734530d991d8e278f4236c61d
Idris Bankole <bankoleidris@gmail.com>
2021-11-16 08:12:05 +0100

    setting enum

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ const App = () => {
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
       />
       <button className="next" onClick={() => nextQuestion()}>
         Next Question
